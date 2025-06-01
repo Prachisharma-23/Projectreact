e06c49be486b4bd2351aaaba048799800d07dd3f
Prachisharma-23 <prachisharmaa2003@gmail.com>
2025-06-01 17:46:33 +0530

    Basic react

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -2,18 +2,13 @@
 import './App.css'
 import Card from './components/Card'
 
-function App() {
-  let myNum={
-    name: Prachi,
-    age: 21
-  }
-
-  
-
+function App(){
+  let arr=[1,2,3]
   return (
     <>
     <h1 className='bg-black text-pink-500 p-4 rounded-xl'>Tailwind css</h1>
-    <Card Company='tcs' myObject={myNum}/>
+    <Card username="hello" btnText="Click me"/>
+    <Card username="work"/>
 
     </>
   )
--- Tailwind/src/components/Card.jsx
+++ Tailwind/src/components/Card.jsx
@@ -1,18 +1,20 @@
-
-function Card(props) {
-    console.log("prpos",props)
+function Card({username,btnText = "visit me"}) {
+    console.log(username)
   return (
-    <div class="max-w-sm mx-auto bg-white rounded-2xl shadow-md overflow-hidden p-4">
+    <div className="max-w-sm mx-auto rounded-2xl shadow-md overflow-hidden p-4">
   <img
-    class="w-full h-56 object-cover"
+    className="w-full h-56 object-cover"
     src="https://th.bing.com/th/id/OIP.pEeKeUoENMqoN-kR8f8XoQHaFj?w=188&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
     alt="This is sample image"
   />
-  <div class="p-4">
-    <h2 class="text-xl font-semibold mb-2">Tailwind</h2>
-    <p class="text-gray-600">
+  <div className="p-4">
+    <h2 className="text-xl font-semibold mb-2">Tailwind</h2>
+    <p className="text-gray-600">
       A utility-based CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.
     </p>
+    <button className="bg-amber-500 text-black mt-2 inline-flex cursor-pointer items-center text-sm font-semibold ">
+      {btnText}
+    </button>
   </div>
 </div>
   )
